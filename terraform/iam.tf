2de5a0659d8d44d38b8fd0680d5cd50f3efe9e2e
eric <ericgoedtel@users.noreply.github.com>
2026-01-31 20:05:04 -0500

    feat: use oidc instead of api keys (#13)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -38,3 +38,67 @@ resource "aws_iam_role_policy" "role_policy_attachment" {
   role   = aws_iam_role.root_takeout_bucket.id
   policy = data.aws_iam_policy_document.root_takeout_policy.json
 }
+
+resource "aws_iam_openid_connect_provider" "tfc" {
+  url = "https://app.terraform.io"
+  client_id_list = [
+    "aws.workload.identity",
+  ]
+}
+
+resource "aws_iam_user" "amolicob_user" {
+  name = "amolicob_user"
+  path = "/"
+}
+
+data "aws_iam_policy_document" "amolicob_role_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_user.amolicob_user.arn]
+    }
+  }
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.tfc.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "app.terraform.io:aud"
+      values   = ["aws.workload.identity"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "app.terraform.io:sub"
+      values   = ["organization:${var.tfc_organization}:project:${var.tfc_project_name}:workspace:*:run_phase:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "amolicob_role" {
+  name = "amolicob_role"
+  path = "/"
+
+  assume_role_policy = data.aws_iam_policy_document.amolicob_role_assume_role_policy.json
+}
+
+resource "aws_iam_user_policy" "amolicob_user_assume_role_policy" {
+  name = "amolicob_user_assume_role_policy"
+  user = aws_iam_user.amolicob_user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Resource = aws_iam_role.amolicob_role.arn
+      },
+    ]
+  })
+}
